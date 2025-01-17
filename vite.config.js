59ddf5637dfc4a06e3a26e2861133e4b245dbd75
Tanjim Pranto <tanjim.outcast@gmail.com>
2025-01-17 18:22:31 +0200

    updated target url in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://talent-hub-review-system.onrender.com",
         changeOrigin: true,
         secure: false,
       },
