3be754eef41f5e785952a64a71342891a147a983
Tanjim Pranto <tanjim.outcast@gmail.com>
2025-01-18 00:15:24 +0200

    health

--- backend/server.js
+++ backend/server.js
@@ -16,3 +16,7 @@ app.use("/api/reviews", require("./routes/reviewRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/healthcheck", (req, res) => {
+  res.status(200).send("OK");
+});
